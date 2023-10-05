a9ac289dba0fc0dfcd172305961d389e4c706f12
Miranda Lucas <mirandalucas@Mirandas-MacBook-Pro.local>
2023-10-05 13:05:46 -0500

    completed workshop

---
+++ practice.js
@@ -0,0 +1,23 @@
+// Pop up message for user
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+alert(message)
+
+// Combination calculation
+let firstNumber = 9 + 1
+console.log(firstNumber)
+
+let secondNumber = 45 - 5
+console.log(secondNumber)
+
+let thirdNumber = 78 / 2
+console.log(thirdNumber)
+
+// Second pop up message with combo
+alert(`${firstNumber} - ${secondNumber} - ${thirdNumber}`)
+
+
+
+
+
+
+
